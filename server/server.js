168156807db09e3e72d6020e96b844c69eb7eef8
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2019-03-25 22:34:19 +0000

    Added BE routes for users and projects

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const users = require("./routes/users");
+const projects = require("./routes/projects");
+const bodyParser = require("./body-parser");
 const port = 3000;
+
+app.use(bodyParser.json());
+//user routes
+app.use("/users", users);
+//project routes
+app.use("/projecst", projects);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
