83fa87b7eee18634b0c9553c5f07fb5cedd6431d
manu-4216 <manu4216@gmail.com>
2019-03-26 21:34:28 +0000

    Link front end and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    fetch("/projects")
+      .then(data => {
+        if (data.status === 200) {
+          return data.json();
+        } else {
+          throw Error(data.statusText);
+        }
+      })
+      .then(data => {
+        // TODO: Use this data
+        console.log(data);
+      })
+      .catch(error => {
+        console.warn("Error while getting the projects:", error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
