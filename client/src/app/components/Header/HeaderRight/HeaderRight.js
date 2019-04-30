1528c286298062fe79df09617fc3097ae424427c
manu-4216 <manu4216@gmail.com>
2019-04-30 21:37:37 +0100

    Enable navigation in the header

--- client/src/app/components/Header/HeaderRight/HeaderRight.js
+++ client/src/app/components/Header/HeaderRight/HeaderRight.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classNames from "classnames";
 import styles from "./HeaderRight.module.css";
 
 const HeaderRight = ({ direction }) => (
   <div className={styles.container}>
     <ul className={classNames(styles.list, styles[direction])}>
-      <li>Projects</li>
-      <li>About Us</li>
+      <li>
+        <Link to="/projects">Projects</Link>
+      </li>
+      <li>
+        <Link to="/about-us">About Us</Link>
+      </li>
     </ul>
   </div>
 );
