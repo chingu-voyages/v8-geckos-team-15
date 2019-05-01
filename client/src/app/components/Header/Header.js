7ca83e075051110973f69ca24d943e59543df0a6
Manuel Micu <manu4216@gmail.com>
2019-05-01 21:31:53 +0100

    Enable navigation in the header (#37)

--- client/src/app/components/Header/Header.js
+++ client/src/app/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import HamburgerMenu from "./MobileMenu/Hamburger";
 import SearchBar from "./SearchBar/SearchBar";
 import HeaderRight from "./HeaderRight/HeaderRight";
+import { Link } from "react-router-dom";
 
 import styles from "./header.module.css";
 
@@ -9,10 +10,10 @@ export const Header = () => (
   <div className={styles.header}>
     <HamburgerMenu />
 
-    <div className={styles.logo}>
+    <Link className={styles.logo} to="/">
       <h1 className={styles.logoTitle}>ProjectCode</h1>
       <i className="fas fa-code logo" />
-    </div>
+    </Link>
 
     <div className={styles.navContainer}>
       <SearchBar />
