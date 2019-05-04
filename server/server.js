4f1bd51656b611132f400162356319a0880c18ff
manu-4216 <manu4216@gmail.com>
2019-05-04 14:52:25 +0100

    Remove logging

--- server/server.js
+++ server/server.js
@@ -2,22 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const fs = require("fs");
-const morgan = require("morgan");
-var path = require("path");
-
 const users = require("./routes/users");
 const projects = require("./routes/projects");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 4000;
 require("dotenv").config();
 const dbPassword = process.env.dbPassword;
-
-// create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(path.join(__dirname, "server.log"), { flags: "a" });
-
-// setup the logger
-app.use(morgan("combined", { stream: accessLogStream }));
 
 const URI = `mongodb://user1:${dbPassword}@ds111455.mlab.com:11455/v8-chingu-geckos-15`;
 
