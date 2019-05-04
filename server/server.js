a6bf2b8cea1c5a5da61b49deec29a16edfaa29f3
manu-4216 <manu4216@gmail.com>
2019-05-04 15:29:36 +0100

    Fix path in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const path = require("path");
 
 const users = require("./routes/users");
 const projects = require("./routes/projects");
